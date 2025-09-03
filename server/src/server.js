d9650ad1ed645b8514d3d6396c65d6afe0b6ee5f
Shreyash Anand <anandshreyash747@gmail.com>
2025-09-03 19:00:20 +0530

    deploy

--- server/src/server.js
+++ server/src/server.js
@@ -20,7 +20,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://pixelchat-1.onrender.com/", // Replace with your actual Vercel URL
     methods: ["GET", "POST"]
   }
 });
